78db6fbbbc841e8c76d4ba581cd06b2386c51a7f
CHeatiiT <161832522+Shivamborn2shine@users.noreply.github.com>
2025-08-07 17:18:52 +0530

    Add files via upload

---
+++ 7 August/reverse using recursion.c
@@ -0,0 +1,22 @@
+//reverse a number using recursion
+#include <stdio.h>
+
+int reverseNumber(int num, int rev) {
+    if (num == 0)
+        return rev;
+    else
+        return reverseNumber(num / 10, rev * 10 + num % 10);
+}
+
+int main() {
+    int number, reversed;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    reversed = reverseNumber(number, 0);
+
+    printf("Reversed number: %d\n", reversed);
+
+    return 0;
+}
